dd6f9ab8905d5daca64e0240a4ea62817034045b
Kyle Dinh <kyledinh@gmail.com>
2018-01-24 12:13:14 -0800

    go fmt

--- go/model/contact.go
+++ go/model/contact.go
@@ -1,16 +1,14 @@
 package model
 
-import (
-
-)
+import ()
 
 type Contact struct {
-	First_name				string	`json:"first_name"`
-	Last_name				string	`json:"last_name"`
-	Email					string	`json:"email"`
-	Phone_number			string	`json:"phone_number"`
-	Street_address			string	`json:"street_address"`
-	City					string	`json:"city"`
-	State					string	`json:"state"`
-	Zipcode					string	`json:"-"`
+	First_name     string `json:"first_name"`
+	Last_name      string `json:"last_name"`
+	Email          string `json:"email"`
+	Phone_number   string `json:"phone_number"`
+	Street_address string `json:"street_address"`
+	City           string `json:"city"`
+	State          string `json:"state"`
+	Zipcode        string `json:"-"`
 }
